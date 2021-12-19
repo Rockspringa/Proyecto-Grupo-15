b27370b6d0f3e7a5053bfd373b186d42e6f9a55d
Dylan <dylan.daev@gmail.com>
2021-12-18 22:32:38 -0600

    Intento de endpoints

--- node_project/index.js
+++ node_project/index.js
@@ -1,43 +1,27 @@
 "use strict";
 
-const Usuario = require("./model.js");
-const bodyParser = require("body-parser");
+// const Usuario = require("./model.js");
+// const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 const port = 8080;
 
 const dirBase = `${__dirname}/views`;
-const userList = []; // temporal - cambiar por base de datos
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false })); // aun no se hace uso de json
+// app.use(bodyParser.json());
 
 app.use(express.static(dirBase));
 app.use("./css", express.static(`${dirBase}/css`));
 app.use("./js", express.static(`${dirBase}/js`));
 
-app.post("/users", (req, res) => {
-    let { nombre, correo, contraseña } = req.body;
+// app.post("/users", (req, res) => {
+//     // Actualizar funcion
+// });
 
-    let exists = userList.find(
-        (user) => user.nombre === nombre && user.contraseña === contraseña
-    );
-    if (!exists) {
-        userList.push(new Usuario(nombre, correo, contraseña));
-        res.status(200).send({ usuario: nombre, estado: "registrado" });
-    } else {
-        res.status(400).send("El usuario ya esta registrado.");
-    }
-});
-
-app.post("/users/login", (req, res) => {
-    let { nombre, contraseña } = req.body;
-    console.log(userList);
-    let exists = userList.find(
-        (user) => user.nombre === nombre && user.contraseña === contraseña
-    );
-    res.send(exists !== undefined);
-});
+// app.post("/users/login", (req, res) => {
+//     // Actualizar funcion
+// });
 
 app.listen(port, () =>
     console.log("> Server is up and running on port : " + port)
--- node_project/views/js/script.js
+++ node_project/views/js/script.js
@@ -37,69 +37,20 @@ document.querySelectorAll(".col > .row").forEach((row) => {
 // funcion para la pagina de registro
 let btn = document.getElementById("sign-up-btn");
 if (btn) {
-    btn.addEventListener("click", async () => {
-        let repeat = document.getElementById("repeat").value;
-        let body = {
-            nombre: document.getElementById("nombre").value,
-            correo: document.getElementById("correo").value,
-            contraseña: document.getElementById("contraseña").value,
-        };
-
-        if (body.nombre || body.correo || body.contraseña) {
-            if (body.contraseña === repeat) {
-                let res = await fetch(`/users`, {
-                    method: "POST",
-                    body: JSON.stringify(body),
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                });
-
-                let data = await res.json();
-                console.log(data);
-            } else {
-                alert(
-                    `Las contraseñas no coinciden. ${body.contraseña} no es igual a ${repeat}`
-                );
-            }
-        } else {
-            document.querySelectorAll(".col > .row").forEach((row) => {
-                const input = row.children[0];
-                const label = row.children[1];
-
-                doShake(input, label);
-            });
-        }
-    });
+    console.log([
+        "No debe de existir el usuario que se creara.",
+        "El administrador debe de haberse identificado con anterioridad",
+    ]);
 }
 
 // funcion para la pagina de login
-btn = document.getElementById("login-btn");
-if (btn) {
-    btn.addEventListener("click", async () => {
-        let body = {
-            nombre: document.getElementById("nombre").value,
-            contraseña: document.getElementById("contraseña").value,
-        };
+// btn = document.getElementById("login-btn");
+// if (btn) {
+//     este no es un caso de uso pero es indispensable para la seguridad
+//     console.log([
+//         "El usuario a ingresar debe de existir en el sistema.",
+//         "No debe de estar ningun usuario identificado en el dispositivo desde el cual ingresa",
+//     ]);
+// }
 
-        if (body.nombre || body.contraseña) {
-            let res = await fetch(`/users/login`, {
-                method: "POST",
-                body: JSON.stringify(body),
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            });
 
-            let data = await res.json();
-            console.log(data);
-        } else {
-            document.querySelectorAll(".col > .row").forEach((row) => {
-                const input = row.children[0];
-                const label = row.children[1];
-
-                doShake(input, label);
-            });
-        }
-    });
-}
